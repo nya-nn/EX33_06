d7d5e3a19c3c4b0ada29f616e41ca97bbb9bc707
椋太 及川 <fantasyinfantoon@gmail.com>
2023-01-26 13:59:44 +0900

    要素の追加

--- EX33_hint/Program.cs
+++ EX33_hint/Program.cs
@@ -8,6 +8,7 @@ namespace EX33_hint
         {
             RectAngle rectAngle1 = new RectAngle(3, 5);
             RectAngle rectAngle2 = new RectAngle(5, 3);
+            RectAngle rectAngle3 = rectAngle1 + rectAngle2;
             if (rectAngle1 == rectAngle2)
             {
                 Console.WriteLine("rectAngle1とrectAngle2は等しい");
@@ -16,6 +17,7 @@ namespace EX33_hint
             {
                 Console.WriteLine("rectAngle1とrectAngle2は等しくない");
             }
+            Console.WriteLine($"2つの長方形が入る最小の長方形は横{rectAngle3.width},縦{rectAngle3.height}");
         }
     }
 }
--- EX33_hint/RectAngle.cs
+++ EX33_hint/RectAngle.cs
@@ -33,10 +33,81 @@ namespace EX33_hint
                 || rectAngle1.width == rectAngle2.height
                 && rectAngle1.height == rectAngle2.width;
         }
-        
+
         public static bool operator !=(RectAngle rectAngle1, RectAngle rectAngle2)
         {
             return !(rectAngle1 == rectAngle2);
+        }
+
+        public static RectAngle operator +(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            float square = 0;
+            float minSquare = 0;
+            float squWidth = 0;
+            float squHeight = 0;
+            float minWidth = 0;
+            float minHeight = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        squWidth = rectAngle1.width + rectAngle2.width;
+                        if (rectAngle1.height >= rectAngle2.height)
+                        {
+                            squHeight = rectAngle1.height;
+                        }
+                        else
+                        {
+                            squHeight = rectAngle2.height;
+                        }
+                        square = squWidth * squHeight;
+                        break;
+                    case 1:
+                        squWidth = rectAngle1.width + rectAngle2.height;
+                        if (rectAngle1.height >= rectAngle2.width)
+                        {
+                            squHeight = rectAngle1.height;
+                        }
+                        else
+                        {
+                            squHeight = rectAngle2.width;
+                        }
+                        square = squWidth * squHeight;
+                        break;
+                    case 2:
+                        squWidth = rectAngle1.height + rectAngle2.width;
+                        if (rectAngle1.width >= rectAngle2.height)
+                        {
+                            squHeight = rectAngle1.width;
+                        }
+                        else
+                        {
+                            squHeight = rectAngle2.height;
+                        }
+                        square = squHeight * squWidth;
+                        break;
+                    case 3:
+                        squWidth = rectAngle1.height + rectAngle2.height;
+                        if (rectAngle1.width >= rectAngle2.width)
+                        {
+                            squHeight = rectAngle1.width;
+                        }
+                        else
+                        {
+                            squHeight = rectAngle2.width;
+                        }
+                        square = squWidth * squHeight;
+                        break;
+                }
+                if (minSquare >= square || minSquare == 0)
+                {
+                    minSquare = square;
+                    minWidth = squWidth;
+                    minHeight = squHeight;
+                }
+            }
+            return new RectAngle(minWidth, minHeight);
         }
     }
 }
